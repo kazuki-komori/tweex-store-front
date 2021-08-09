9a9b18ccc4497fac630b963eac7f844317c7d7d8
kazuki-komori <komokazu.nankai@gmail.com>
2021-08-09 15:35:54 +0900

    setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const path = require("path")
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias["@"] = path.join(__dirname + "/src")
+    return config;
+  },
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { AppHeader } from "@/components/layout/AppHeader" 
+
+export const Home = () => {
+  return (
+    <div>
+      <AppHeader />
+    </div>
+  )
+}
+
+export default Home
